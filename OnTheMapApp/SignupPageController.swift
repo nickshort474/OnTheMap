05b6c0ab87aea7f4f07a74f8ba323d9d6b7b7ff6
Nick Short <nickshorty2010@yahoo.co.uk>
2015-10-28 10:52:32 +0000

    add signup page

---
+++ OnTheMapApp/SignupPageController.swift
@@ -0,0 +1,30 @@
+//
+//  SignupPageController.swift
+//  OnTheMapApp
+//
+//  Created by Nick Short on 28/10/2015.
+//  Copyright © 2015 Nick Short. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignupPageController:UIViewController{
+    
+    @IBOutlet weak var signupWebview: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string:"https://www.udacity.com/account/auth#!/signup")
+        let request = NSURLRequest(URL: url!)
+        signupWebview.loadRequest(request)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
